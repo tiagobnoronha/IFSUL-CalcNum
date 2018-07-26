688f6fc8ca8be210dd284b6253297793d5b7c7b8
Tiago Baptista Noronha <tiago.bn@me.com>
2018-07-26 09:18:12 -0300

    Initial Commit

---
+++ IFSUL-CalcNum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IFSUL-CalcNum
+//
+//  Created by Tiago Baptista Noronha on 26/07/2018.
+//  Copyright © 2018 Tiago Baptista Noronha. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
